23ede644dd7f56b12f85856eda09d771747f9634
MinhNhatA3 <88643204+Rainsh0wer@users.noreply.github.com>
2025-02-14 17:47:35 +0700

    Update Skip-Peer-Review.js
    fix button

--- Skip-Peer-Review.js
+++ Skip-Peer-Review.js
@@ -1,16 +1,20 @@
+//coursera doi ID button nen fix lai
+
 const radioGroups = document.querySelectorAll('div[role="radiogroup"]');
 
 radioGroups.forEach(group => {
   const radioButtons = group.querySelectorAll('input[type="radio"]');
   
   let highestPointsButton = null;
+  let highestPoints = -Infinity;
 
   radioButtons.forEach(button => {
-    const label = button.nextElementSibling;
-    if (label && label.textContent.includes('pt')) {
-      const pointValue = parseInt(label.textContent.trim().replace('pt', '').trim());
+    const labelTextElement = button.closest('label').querySelector('.css-1rlln5c span');
+    if (labelTextElement && labelTextElement.textContent.includes('points')) {
+      const pointValue = parseInt(labelTextElement.textContent.replace('points', '').trim(), 10);
       
-      if (!highestPointsButton || pointValue > parseInt(highestPointsButton.nextElementSibling.textContent.trim().replace('pt', '').trim())) {
+      if (pointValue > highestPoints) {
+        highestPoints = pointValue;
         highestPointsButton = button;
       }
     }
